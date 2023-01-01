ab2bf3e896dfe4be5bfccf50dc074701e9547c06
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-01 13:44:17 +0100

    :wrench:

---
+++ src/pages/Index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+    return (
+        <div className="body_container">
+            <div class="title">
+                <h1>HRnet</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Index
