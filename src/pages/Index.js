bf8dc810f56557d9b4f1c93b6594cdd4d3061e58
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-01 18:35:32 +0100

    :wrench:

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const Index = () => {
     return (
         <div className="body_container">
             <div class="title">
                 <h1>HRnet</h1>
+            </div>
+            <div className="main_infos">
+                <Link className="main-nav-logo" to="/employee">
+                    <h1 className="sr-only">View Current Employees</h1>
+                </Link>
+                <h2>Create Employee</h2>
             </div>
         </div>
     )
