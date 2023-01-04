5294a066864d2babac07cf4bfd7f576f2293b6c5
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-04 22:17:17 +0100

    :wrench:

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,25 +1,75 @@
 import { useSelector } from 'react-redux'
-import { useContext } from 'react'
+import DataTable from 'react-data-table-component'
 
 function EmployeesList() {
     const employees = useSelector((state) => state.employees)
+    const columns = [
+        {
+            name: 'First Name',
+            selector: (row) => row.firstName,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'Last Name',
+            selector: (row) => row.lastName,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'Date Of Birth',
+            selector: (row) => row.dateOfBirth,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'Start Date',
+            selector: (row) => row.startDate,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'Department',
+            selector: (row) => row.department,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'Street',
+            selector: (row) => row.street,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'State',
+            selector: (row) => row.state,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'City',
+            selector: (row) => row.city,
+            sortable: true,
+            filterable: true,
+        },
+        {
+            name: 'Zip Code',
+            selector: (row) => row.zipCode,
+            sortable: true,
+            filterable: true,
+        },
+    ]
 
     return (
-        <ul>
-            {employees.map((employee) => (
-                <li>
-                    <h3>
-                        {employee.firstName} {employee.lastName}
-                        {employee.startDate}
-                    </h3>
-                    <p>
-                        Address: {employee.street}, {employee.city},{' '}
-                        {employee.state}, {employee.zipCode}
-                    </p>
-                    <p>Department: {employee.department}</p>
-                </li>
-            ))}
-        </ul>
+        <DataTable
+            title="Current Employees"
+            columns={columns}
+            data={employees}
+            pagination
+            highlightOnHover
+            responsive
+            defaultSortFieldId={1}
+        />
     )
 }
 export default EmployeesList
