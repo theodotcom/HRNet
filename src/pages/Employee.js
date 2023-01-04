034757c704671f5963ce0cdbea2bb7b282f8df2d
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-04 15:19:34 +0100

    store v1 test :wrench:

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useStore } from 'react-redux'
 
 const Employee = () => {
+    const store = useStore()
+    const state = useSelector((state) => state)
     return (
         <div className="body">
             <Link className="main-nav" to="/">
                 <h1 className="sr-only">Home</h1>
             </Link>
-            <h1>Current Employees</h1>
+            <h1>Current Employees {state.firstName}</h1>
         </div>
     )
 }
