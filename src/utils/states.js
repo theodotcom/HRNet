ceceb4412fa99aa85fc88f68bcae88544d9a39d5
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-06 14:17:12 +0100

    US states :bento:

---
+++ src/utils/states.js
@@ -0,0 +1,19 @@
+const states = [
+    { label: 'Alabama', value: 'AL' },
+    { label: 'Alaska', value: 'AK' },
+    { label: 'American Samoa', value: 'AS' },
+    { label: 'Arizona', value: 'AZ' },
+    { label: 'Arkansas', value: 'AR' },
+    { label: 'California', value: 'CA' },
+    { label: 'Colorado', value: 'CO' },
+    { label: 'Connecticut', value: 'CT' },
+    { label: 'Delaware', value: 'DE' },
+    { label: 'District Of Columbia', value: 'DC' },
+    { label: 'Federated States Of Micronesia', value: 'FM' },
+    { label: 'Florida', value: 'FL' },
+    { label: 'Georgia', value: 'GA' },
+    { label: 'Guam', value: 'GU' },
+    { label: 'Hawai', value: 'HI' },
+    { label: 'Texas', value: 'TX' },
+]
+export { states }
