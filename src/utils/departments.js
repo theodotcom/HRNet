cbcc1a8d1182999e26f9097ef07e532f2000a9d1
theodotcom <61735898+theodotcom@users.noreply.github.com>
2023-01-07 15:53:02 +0100

    departments & colums :bento:

---
+++ src/utils/departments.js
@@ -0,0 +1,9 @@
+const departments = [
+    { label: 'Sales', value: 'Sales' },
+    { label: 'Marketing', value: 'Marketing' },
+    { label: 'Engineering', value: 'Engineering' },
+    { label: 'Human Resources', value: 'Human Resources' },
+    { label: 'Legal', value: 'Legal' },
+]
+
+export { departments }
---
+++ src/utils/gridColumns.js
@@ -0,0 +1,13 @@
+const columns = [
+    { field: 'firstName', headerName: 'First Name', width: 150 },
+    { field: 'lastName', headerName: 'Last Name', width: 150 },
+    { field: 'dateOfBirth', headerName: 'Date Of Birth', width: 150 },
+    { field: 'startDate', headerName: 'Start Date', width: 150 },
+    { field: 'department', headerName: 'Department', width: 150 },
+    { field: 'street', headerName: 'Street', width: 150 },
+    { field: 'state', headerName: 'State', width: 150 },
+    { field: 'city', headerName: 'City', width: 150 },
+    { field: 'zipCode', headerName: 'Zip Code', width: 150 },
+]
+
+export { columns }
